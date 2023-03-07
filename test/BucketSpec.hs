b209ac74248bbf142f4f63f5f89e637843f9696a
Koray Al <koray.al@soostone.com.tr>
2023-03-07 10:32:40 +0300

    add package.yaml and switch to Tasty

--- test/BucketSpec.hs
+++ test/BucketSpec.hs
@@ -6,43 +6,37 @@ module BucketSpec where
 import           Control.Concurrent
 import           Control.Monad
 import           Data.IORef
-import           Test.Hspec
-import           Test.HUnit
+import           Test.Tasty.Hspec
+import           Test.Tasty.HUnit
 -------------------------------------------------------------------------------
 import qualified Stoplight.Bucket   as T
 -------------------------------------------------------------------------------
 
-spec :: Spec
-spec = parallel $ describe "throttle" $ do
-
+spec_throttle :: Spec
+spec_throttle = parallel $ describe "throttle" $ do
   forM_ [1000, 10000, 100000, 250000] $ \ tick ->
     forM_ [1, 5, 20, 40, 50] $ \ regen ->
       it ("should work at " ++ show (tick,regen)) $ do
         testThrottle tick regen
+  where
+    testThrottle tick regen =  do
+        i <- newIORef (0 :: Integer)
+        t <- T.new 0 50 tick regen
 
+        replicateM_ 20 $ forkIO $ forever $ do
+          T.wait t 1
+          atomicModifyIORef' i $ \ i' -> (i'+1, ())
 
+        -- wait a multiple of tick
+        let w = tick * 20
 
-testThrottle tick regen =  do
-    i <- newIORef 0
-    t <- T.new 0 50 tick regen
+        threadDelay w
+        cnt <- readIORef i
 
-    replicateM 20 $ forkIO $ forever $ do
-      T.wait t 1
-      atomicModifyIORef' i $ \ i -> (i+1, ())
+        let maxLim = (fromIntegral w / fromIntegral tick) * fromIntegral regen
+            (cnt' :: Double) = fromIntegral cnt
 
-    -- wait a multiple of tick
-    let w = tick * 20
-
-    threadDelay w
-    cnt <- readIORef i
-
-    let maxLim = (fromIntegral w / fromIntegral tick) * fromIntegral regen
-        cnt' = fromIntegral cnt
-
-    assertBool ("meets upperbound: " ++ show cnt') $ cnt' <= maxLim
-    assertBool ("meets lowerbound: " ++ show cnt') $ cnt' >= maxLim * 0.8
-
-
-
+        assertBool ("meets upperbound: " ++ show cnt') $ cnt' <= maxLim
+        assertBool ("meets lowerbound: " ++ show cnt') $ cnt' >= maxLim * 0.8
 
 
